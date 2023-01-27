589e4c5337559aedff869b3d1e67c22bc9b112f6
hstandifer <hunterstandifer7@gmail.com>
2023-01-27 16:29:55 -0600

    adds basic logic for adding meds and displaying data

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
+// import MedInput from "./components/MedInput";
 
 export default function App() {
+  const [medicationsArray, setMedicationsArray] = useState([]);
+  const [medName, setMedName] = useState("");
+  const [medFrequency, setMedFrequency] = useState("");
+  const [medDose, setMedDose] = useState("");
+  const [medTime, setMedTime] = useState("");
+
+  function addMedsHandler() {
+    setMedicationsArray((currentMedicationsArray) => [
+      {
+        name: medName,
+        id: Math.random().toString(),
+        frequency: medFrequency,
+        dosage: medDose,
+        time: medTime,
+      },
+      ...currentMedicationsArray,
+    ]);
+  }
+
+  function handleStateChange(id, text) {
+    if (id === "1") {
+      setMedName(text);
+    } else if (id === "2") {
+      setMedFrequency(text);
+    } else if (id === "3") {
+      setMedDose(text);
+    } else {
+      setMedTime(text);
+    }
+  }
+
+  console.log(medName, medFrequency, medDose, medTime);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder={"Medication Name"}
+          value={medName}
+          onChangeText={handleStateChange.bind(this, "1")}
+        />
+        <TextInput
+          placeholder={"Medication Frequency"}
+          value={medFrequency}
+          onChangeText={handleStateChange.bind(this, "2")}
+        />
+        <TextInput
+          placeholder={"Medication Dose"}
+          value={medDose}
+          onChangeText={handleStateChange.bind(this, "3")}
+        />
+        <TextInput
+          placeholder={"Medication Time"}
+          value={medTime}
+          onChangeText={handleStateChange.bind(this, "4")}
+        />
+      </View>
+
+      <Button title="Add Med" onPress={addMedsHandler} />
+      <View style={styles.medContainer}>
+        <FlatList
+          data={medicationsArray}
+          renderItem={({ item }) => (
+            <Text>
+              {item.name},{item.id},{item.dosage},{item.time}
+            </Text>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  inputContainer: {
+    backgroundColor: "red",
+  },
+  medContainer: {
+    backgroundColor: "yellow",
+    color: "white",
   },
 });
