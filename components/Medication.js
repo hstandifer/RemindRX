6166a794ab68c2649ad3e61bcea0333c71edcc8c
Luis <eternal_pitta0b@icloud.com>
2023-05-19 20:28:19 -0500

    update UI and apply style

--- components/Medication.js
+++ components/Medication.js
@@ -1,13 +1,19 @@
 import { Pressable, View, Text, StyleSheet, Button } from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 export default function Medication({ medInfo }) {
   return (
     <View style={styles.medContainer}>
       <Pressable>
-        <View style={styles.itemContainer}>
-          <Text style={styles.medItem}>
-            {medInfo.name},{medInfo.dose},{medInfo.frequency},
-            {medInfo.time}
+        <View style={[styles.itemContainer]}>
+          <Text style={[styles.medItem, styles.title]}>{medInfo.name}</Text>
+          <View style={styles.timeContainer}>
+            <FontAwesome5 name={"clock"} solid size={15} color={"black"} />
+            <Text style={styles.medItem}>{medInfo.time}</Text>
+          </View>
+
+          <Text style={[styles.medItem, styles.badge]}>
+            Pending
           </Text>
         </View>
       </Pressable>
@@ -19,16 +25,51 @@ const styles = StyleSheet.create({
   itemContainer: {
     margin: 5,
     borderRadius: 8,
-    height: 150,
-    width: 150,
-    backgroundColor: "purple",
+    height: 170,
+    width: 170,
+    backgroundColor: "#F8F9FAFF",
+    borderColor: "#BDC1CAFF",
+    borderWidth: 2,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  // if the user missed to take a med, apply this style
+  redBorder: {
+    borderColor: "red",
   },
   medItem: {
-    color: "white",
-    padding: 5,
+    color: "black",
+    padding: 2,
+    textAlign: "center",
   },
   medContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
+  },
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+  },
+  timeContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  badge: {
+    width: 75,
+    borderRadius: 20,
+    margin: 15,
+    paddingVertical: 5,
+    backgroundColor: "#BDC1CAFF",
+  },
+  badgeGreen: {
+    backgroundColor: "#EEFDF3FF",
+    color: "#117B34FF",
+  },
+  badgeRed: {
+    backgroundColor: "#FEF0F1FF",
+    color: "#F22128FF",
   },
 });
